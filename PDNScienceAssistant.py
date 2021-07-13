7fa59a46f14002735ee76db32b24b07760672be3
Pathos315 <john.fallot@gmail.com>
2021-07-13 17:11:19 -0400

    Includes updates from michele's fork
    Optimization features, although known issues include pandas not playing along nicely

--- PDNScienceAssistant.py
+++ PDNScienceAssistant.py
@@ -22,40 +22,34 @@ def main(folder, file, num_of_files): #directory = dir, file = starting at, num_
     full_dir = [file for file in os.listdir(folder) if file.endswith('.pdf')] 
     '''Only gets the pdfs in the folder and will overlook non-pdf files.'''
     
-    binder = len(full_dir) 
     '''Maximum number of pdfs in a folder.'''
-    if num_of_files == 0:
-        num_of_files = binder
-    '''If num_of_file arg is 0, gets all the files.'''
+    if num_of_files == 0 or num_of_files > len(full_dir): 
+        num_of_files = len(full_dir)
+    '''If num_of_file arg is 0, or if it is larger than the number of files in the target folder, we set it to the number of files in the folder.'''
     
-    compendium = []
+    files_to_check = full_dir[:num_of_files] #select the first num_of_files elements from the list
     '''This gets passed to the dataframe'''
 
-    for file in range(num_of_files):
-        if file <= num_of_files:
-            try:
-                print("Preparing file extraction...")
-                filepath = str(full_dir[file])
-                filename = os.path.join(folder,filepath)
-                '''Generates the path to each pdf file'''
-                print("\nBeginning extraction... \n")
-                compendium_item = extract(filename, file, num_of_files, filepath)
-                compendium.append(compendium_item)
-                '''The extraction process for each file gets appended to the compendium'''
-                print(f"\nPackaging {filepath}... \n")
-                print("\nChecking for next file... \n")
-                continue
-            except:
-                pass
+    for file_index,file in enumerate(files_to_check): #note: now the variable file is an element of files_to_check, not a numerical index
+        try:
+            print("\nPreparing file extraction...\n")
+            filepath = str(file)
+            filename = os.path.join(folder,filepath)
+            '''Generates the path to each pdf file'''
+            print("\nBeginning extraction... \n")
+            print(f"\nProcessing file {file_index} of {num_of_files} | {filepath}\n", end = "\r")
+            extract(filename, filepath)
+            '''The extraction process for each file gets appended to the compendium'''
+            print(f"\nPackaging {filepath}... \n")
+            print("\nChecking for next file... \n")
+            continue
+        except:
+            pass
 
-        elif file == num_of_files:
-            break
-        '''This guarantees that the program will iterate for every pdf in the folder. I've been told to write this as an enumerate function, but that seems to throw up problems.'''
-
-    printing(compendium)
     '''Once the entire folder has been extracted, the dataframing process begins.'''
+    finalize(compendium)
 
-def extract(filename, file, num_of_files, filepath):
+def extract(filename, filepath):
     '''Using the supplied args, each file is opened using pdfplumber, which converts the pdf copy into a string.'''
     doi_results = pdf2doi(filename, verbose=True, save_identifier_metadata = True, filename_bibtex = False)
 
@@ -69,27 +63,29 @@ def extract(filename, file, num_of_files, filepath):
         if page <= n:
             findings = study.pages[page].extract_text()
             '''Each page's string gets appended to preprint []'''
-            print(f"Processing file {file} of {num_of_files} | {filepath} | Page {page} of {n}...", end = "\r")
+            print(f" Processing Page {page} of {n}...", end = "\r")
             preprints.append(findings) 
             continue
             '''Proceeds to next page in the study'''
 
-        elif page == n:
+        elif page > n:
             study.close()
             '''After each page in the study is extracted, closes the study.'''
             break
     
     for preprint in preprints:
-        manuscript = str(preprint).strip()
-
-        '''Tokenization & Keyword search. Stopwords are filtered out.'''
-        postprint = redaction(manuscript)
+        '''
+        Tokenization & Keyword search. Stopwords are filtered out.
+        '''
+        postprint = redaction(preprint)
         all_words = tokenization(postprint)
         wordscore, target_word_overlap, bycatch_word_overlap, research_word_overlap, fdist_top5 = word_match(all_words)
         fdist_top5 = frequency(all_words)
 
+        '''
+        Below is what gets passed back from the entire extraction process.
+        '''
         compendium_item = {
-        '''This is what gets passed back from the entire extraction process.'''
 
             'Title': filepath,
             'DOI': doi_results['identifier'],
@@ -100,12 +96,15 @@ def extract(filename, file, num_of_files, filepath):
             'Bycatch Words': bycatch_word_overlap,
             'Wordscore' : wordscore
 
-        }
+            }
+        
+        compendium.append(compendium_item)
 
-        return compendium_item
-
-def redaction(manuscript):
-    '''The resulting manuscript is put into lowercase and all non-alphanumeric characters are removed.'''
+def redaction(preprint):
+    '''
+    The resulting manuscript is put into lowercase and all non-alphanumeric characters are removed.
+    '''
+    manuscript = str(preprint).strip()
     manuscript = manuscript.lower()
     postprint = re.sub(r'\W+', ' ', manuscript)
     return postprint
@@ -119,14 +118,16 @@ def tokenization(postprint):
     return all_words
 
 def word_match(all_words):
-    '''The remaining tokenized words are compared against several lists of words, by way of the lambda overlap function 
+
+    '''
+    The remaining tokenized words are compared against several lists of words, by way of the lambda overlap function.
     target_words are the words we're looking for in a study. 
     bycatch_words are words that generally indicate a false positive
     research_words are words that help us determine the underlying study design: was it a randomized control or analytical?
 
     The length of the target_words minus the length of the bycatch words determines the wordscore. A positive wordscore means a paper is more likely than not to be a match, and vice versa.
-
     '''
+
     #TO DO: have target words import from separate txt file
     target_words = ["prosocial", "design", "intervention", "reddit", "humane","social media","user experience","nudge","choice architecture","user interface", "misinformation", "disinformation", "Trump", "conspiracy", "dysinformation", "users"]
     bycatch_words = ["psychology", "pediatric", "pediatry", "autism", "mental", "medical", "oxytocin", "adolescence", "infant", "health", "wellness", "child", "care", "mindfulness"]
@@ -148,31 +149,35 @@ def frequency(all_words):
     fdist_top5 = fdist.most_common(5) #Gets the top 10 most common words
     return fdist_top5
 
-def printing(compendium):
-    '''After the paper is fully processed and exported to the compendium, the compendium is put through a pandas dataframe.
+def finalize(compendium):
+    '''
+    After the paper is fully processed and exported to the compendium, the compendium is put through a pandas dataframe.
     Normally, what the compendium returns is page by page.
-    What we aim to do is consolidate all entries that belong to one paper, which is what the aggregate_functions aims to do (with occasional success).'''
+    What we aim to do is consolidate all entries that belong to one paper, which is what the aggregate_functions aims to do (with occasional success).
+    '''
+    df = create_dataframe(compendium)
+    print (df.head())
+    finish(df)
+
+def create_dataframe(compendium):
+    '''A dataframe is generated using pandas, but TBH this #@!?>_< code has only generated headaches so far.'''
     print(f'\nFinalizing data for human review...\n')
     df = pd.DataFrame(compendium)
-    aggregation_functions = {'DOI': 'first', 'Pages':'first', '5 Most Common Words': 'max', 'Wordscore':'sum'}
-    df_new = df.groupby(df['Title']).aggregate(aggregation_functions)
-    df_new = df_new.sort_values('5 Most Common Words', ascending=False)
-    print(df_new.head())
+    return df
 
-    '''Finally, a YYMMDD timestamp is generated, and a pseudo_unique ID to prevent the deletion of prior work.'''
+def csv_filename():
+    '''A YYMMDD timestamp is generated, and a pseudo_unique ID to prevent the deletion of prior work.'''
     csv_date = now.strftime('%y%m%d')
     export_ID = random.randint(0,100)
     export_name = f'{csv_date}_PDN_studies_{export_ID}.csv'
-    df_new.to_csv(export_name)
+    return export_name
+
+def finish(df):
+    '''The dataframe is handed off to a csv file and exported. A time is measured for benchmarking.'''
+    export_name = csv_filename()
+    df.to_csv(export_name)
     t2 = time.perf_counter()
     print(f'\nExtraction finished in {t2-t1} seconds.\nDataframe exported to {export_name}')
-
+    
 '''This line of code initiates the entire process.'''
 main(dir, 0, 2)
-
-
-
-#ΑαΒβΓγΔδΕεΖζΗηΘθΙιΚκΛλΜμΝνΞξΟοΠπΡρΣσςΤτΥυΦφΧχΨψΩω
-#        
-
-#'All Citations': all_citations,
