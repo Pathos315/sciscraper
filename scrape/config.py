cede98e73ded2a7f33c80dff755e54fcc6d499ce
Pathos315 <john.fallot@gmail.com>
2022-01-20 11:55:21 -0500

    Added docstrings and metaclasses to ScrapeConfig

--- scrape/config.py
+++ scrape/config.py
@@ -1,8 +1,9 @@
 import json
 from dataclasses import dataclass
 
-@dataclass
+@dataclass(frozen=True, order=True)
 class ScrapeConfig:
+    """A dataclass containing the overall configurations"""
     export_dir: str
     log_dir: str
     prime_src: str
@@ -16,6 +17,14 @@ class ScrapeConfig:
 
 
 def read_config(config_file: str) -> ScrapeConfig:
+    """read_config takes a .json file and returns a ScrapeConfig object.
+
+    Args:
+        config_file (str): the path to the .json file containing the configs.
+
+    Returns:
+        ScrapeConfig: A dataclass containing the overall configurations
+    """
     with open(config_file, encoding="utf-8") as file:
         data = json.load(file)
         return ScrapeConfig(**data)
