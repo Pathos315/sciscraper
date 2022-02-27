84ae2122bde6d2f753cc9d9a933d76470c41716e
Pathos315 <john.fallot@gmail.com>
2022-02-27 14:50:29 -0500

    Create textanalyzer.py

---
+++ scrape/textanalyzer.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from typing import Protocol
+
+
+@dataclass(frozen=True, order=True)
+class AnalysisResult:
+    """_summary_"""
+
+    wordscore: int
+    matching_terms: list[tuple[str, int]]
+    mode_words: list[tuple[str, int]]
+    research: list[tuple[str, int]]
+    solution: list[tuple[str, int]]
+    tech: list[tuple[str, int]]
+    digital_object_id: str = "NaN"
+
+
+class TextAnalyser(Protocol):
+    """TextAnalyser _summary_
+
+    Args:
+        Protocol (_type_): _description_
+    """
+
+    def analyze(self, text_query: str) -> AnalysisResult:
+        ...
