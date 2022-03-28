de8e48c298c03affdcaa9894f620e109fdca2071
trag1c <77130613+trag1c@users.noreply.github.com>
2022-03-28 19:10:27 +0200

    Updated the test to match the new function signature

--- test_pdf.py
+++ test_pdf.py
@@ -5,11 +5,11 @@ from scrape.textscraper import compute_filtered_tokens
 
 class TestPdfScraper(unittest.TestCase):
     def test_filtered_tokens_empty(self):
-        self.assertEqual(len(compute_filtered_tokens([])), 0)
+        self.assertEqual(len({*compute_filtered_tokens([])}), 0)
 
     def test_filtered_tokens_stop(self):
         tokens = ["please like and subscribe"]
-        filtered_tokens = compute_filtered_tokens(tokens)
+        filtered_tokens = {*compute_filtered_tokens(tokens)}
         print(tokens)
         print(filtered_tokens)
         self.assertEqual(filtered_tokens, {"please", "like", "subscribe"})
