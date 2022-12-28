ee1de3dab33aade5f094c4dc6b23a89da42bd2cb
Pathos315 <john.fallot@gmail.com>
2022-12-28 14:33:33 -0500

    Modified file architecture

--- src/sciscraper/scrape/log.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-logger = logging.getLogger("sciscraper")
-logger.setLevel(level=logging.INFO)
-formatter = logging.Formatter("\n[sciscraper]: %(asctime)s - %(message)s\n")
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
-logger.addHandler(stream_handler)
