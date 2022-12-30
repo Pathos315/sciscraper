7b2f3177de5906b436ae1606e200a16d5ab7eddb
Pathos315 <john.fallot@gmail.com>
2022-12-30 18:24:27 -0500

    additional wordscore improvements

--- src/sciscrape/wordscore.py
+++ src/sciscrape/wordscore.py
@@ -32,42 +32,92 @@ class RelevanceCalculator:
     neg_part: float
     total_len: int
     implicature_score: float
+    s: float = 0.95
 
     def __call__(self) -> float:
-        """Calculate the relevance of the paper, or abstract, as a percentage.
+        """
+        Calculates the relevance of the paper, or abstract, as a percentage.
 
-        Returns
-        -------
-        float
-            The probability that the paper is relevant, as a percentage.
+        Returns:
+            float: The probability that the paper is relevant, as a percentage.
 
-        Example
-        -------
+        Example:
         >>> calculator = RelevanceCalculator(
                 pos_part=2,
                 neg_part=1,
                 total_len=100,
                 implicature_score=0.9
                 )
-        >>> raw_score = (2 + 1) / (2 + 0.95 + 1)
-        ... = 3 / 3.95
-        ... = 0.76
-        >>> bycatch_coefficient
-        ... = (1 + 1) / (1 + (2 - 0.95))
-        ... = 2 / 1 + 1.05
-        ... = 2 / 2.05
-        ... = 0.97
-        >>> weight = 0.9 * 0.97
-        ... = 0.873
         >>> wordscore = (0.76 + 0.87) / 2
-        ... return 0.815 # or 81.5%
+        0.815 # or 81.5%
         """
-        s: float = 0.95
-        probability = self.pos_part - self.neg_part / self.total_len * 100
-        raw_score = (probability + 1) / (probability + s + 1)
-        bycatch_coefficient = (self.neg_part + 1) / (self.neg_part + (2 - s))
-        weight = self.implicature_score * bycatch_coefficient
+        probability = self.get_probability()
+        raw_score = self.get_raw_score(probability)
+        bycatch_coefficient = self.get_bycatch_coefficient()
+        weight = self.get_weight(bycatch_coefficient)
         return np.mean([raw_score, weight], dtype=float)
+
+    def get_probability(self):
+        """
+        Gets the percent chance that the paper is relevant.
+
+        Returns:
+            float: the percent chance that the paper is relevant.
+
+        Example:
+        >>> probability = (5 - 3) / 20 * 100
+        2 / 20 * 100
+        0.1 * 100
+        10
+        """
+        return (self.pos_part - self.neg_part) + 1 / self.total_len * 100
+
+    def get_raw_score(self, probability: float) -> float:
+        """
+        Gets the preliminary score before it gets weighted.
+
+        Args:
+            probability (float): the percent chance that the paper is relevant.
+
+        Returns:
+            float: The preliminary score before being weighted.
+
+        Example:
+        >>> raw_score = (2 + 1) / (2 + 0.95 + 1)
+        0.76
+        """
+        return (probability + 1) / (probability + self.s + 1)
+
+    def get_bycatch_coefficient(self) -> float:
+        """
+        Gets a probability value, which is determined from the number of bycatch words identified.
+
+        Returns:
+            float: The preliminary score before being weighted.
+
+        Example:
+        >>> bycatch_coefficient = (1 + 1) / (1 + (2 - 0.95))
+        2 / 1 + 1.05
+        2 / 2.05
+        0.97
+        """
+        return (self.neg_part + 1) / (self.neg_part + (2 - self.s))
+
+    def get_weight(self, bycatch_coefficient: float) -> float:
+        """
+        Returns the weight as a result of the implicature score and bycatch coefficient.
+
+        Args:
+            bycatch_coefficient (float): A probability that is determined from the number of bycatch words identified.
+
+        Returns:
+            float: The weight as a result of the implicature score and bycatch coefficient.
+
+        Example:
+        >>> weight = 0.9 * 0.97
+        0.873
+        """
+        return self.implicature_score * bycatch_coefficient
 
     @classmethod
     def from_dict(cls, dict_input: dict):
