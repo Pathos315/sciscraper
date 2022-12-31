67eaac31b74d072ed69c2d087663f7ba870cb178
Pathos315 <john.fallot@gmail.com>
2022-12-31 18:05:58 -0500

    Major changes to wordscore

--- src/sciscrape/wordscore.py
+++ src/sciscrape/wordscore.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass, asdict
+from typing import Optional, Union
 import numpy as np
+from math import comb
 
 
 @dataclass(slots=True, order=True)
@@ -11,113 +13,83 @@ class RelevanceCalculator:
     based on the number of times target and bycatch words appear in it, its length,
     and a prior zero-shot classification score.
 
-    Attributes
-    ----------
-    pos_part : float
-        The number of times a target word appeared in the paper or abstract.
-    neg_part : float
-        The number of times a bycatch word appeared in the paper or abstract.
-    total_len : int
-        The total number of words in the paper or abstract.
-    implicature_score : float
-        The highest score from a prior `DocScraper` zero-shot classification.
+    Attributes:
+        pos_part (float): The number of times a target word appeared in the paper or abstract.
+        neg_part (float): The number of times a bycatch word appeared in the paper or abstract.
+        total_len (int): The total number of words in the paper or abstract.
+        implicature_score (float, Optional): The highest score from a prior `DocScraper` zero-shot classification.
 
-    Methods
-    -------
-    __call__ : returns wordscore value as a float
-
+    Methods:
+        __call__ : returns wordscore value as a float
     """
 
-    pos_part: float
-    neg_part: float
+    pos_part: int
+    neg_part: int
     total_len: int
-    implicature_score: float
-    s: float = 0.95
+    implicature_score: Optional[float]
 
     def __call__(self) -> float:
         """
-        Calculates the relevance of the paper, or abstract, as a percentage.
+        Calculates the relevance of the paper, or abstract, as a percentage, by using a binomial distribution formula.
 
         Returns:
             float: The probability that the paper is relevant, as a percentage.
 
         Example:
-        >>> calculator = RelevanceCalculator(
-                pos_part=2,
-                neg_part=1,
-                total_len=100,
-                implicature_score=0.9
-                )
-        >>> wordscore = (0.76 + 0.87) / 2
+        >>> calculator = RelevanceCalculator(pos_part=2, neg_part=1, total_len=100, implicature_score=0.9)
+        >>> calculator()
         0.815 # or 81.5%
         """
-        probability = self.get_probability()
-        raw_score = self.get_raw_score(probability)
-        bycatch_coefficient = self.get_bycatch_coefficient()
-        weight = self.get_weight(bycatch_coefficient)
-        return np.mean([raw_score, weight], dtype=float)
+        # The bycatch is added to the total length,
+        # in order to make the presense of bycatch words negatively impactful.
+        weighted_length: int = self.total_len + self.neg_part
+        neutral_part: int = weighted_length - self.pos_part
+        # start binomial distribution i.e. (n! / (x! * (n-x)!) * (x/n)^x * ((n-x)/n)^(n-x))
+        choice: int = comb(weighted_length, self.pos_part)
+        probability: float = self.get_probability(self.pos_part, weighted_length)
+        failure_prob: float = self.get_probability(neutral_part, weighted_length)
+        # The highest zero shot classification score accounts for about 20% of the final wordscore
+        raw_probability: float = 1 - (choice * probability * failure_prob)
+        return self.calculate_wordscore(raw_probability)
 
-    def get_probability(self):
+    def calculate_wordscore(self, raw_probability: float) -> float:
         """
-        Gets the percent chance that the paper is relevant.
-
-        Returns:
-            float: the percent chance that the paper is relevant.
-
-        Example:
-        >>> probability = (5 - 3) / 20 * 100
-        2 / 20 * 100
-        0.1 * 100
-        10
-        """
-        return (self.pos_part - self.neg_part) + 1 / self.total_len * 100
-
-    def get_raw_score(self, probability: float) -> float:
-        """
-        Gets the preliminary score before it gets weighted.
+        Calculates the final word score based on the raw probability and the implicature score.
 
         Args:
-            probability (float): the percent chance that the paper is relevant.
+            raw_probability (float): The raw probability that the paper is relevant.
 
         Returns:
-            float: The preliminary score before being weighted.
+            float: The final word score, as a percentage.
 
         Example:
-        >>> raw_score = (2 + 1) / (2 + 0.95 + 1)
-        0.76
+        >>> calculator = RelevanceCalculator(pos_part=2, neg_part=1, total_len=100, implicature_score=0.9)
+        >>> calculator.calculate_wordscore(0.6)
+        0.74 # or 74%
         """
-        return (probability + 1) / (probability + self.s + 1)
+        weight = 0.8
+        self.implicature_score = (
+            0.5 if self.implicature_score is None else self.implicature_score
+        )
+        return (raw_probability * weight) + (self.implicature_score * (1 - weight))
 
-    def get_bycatch_coefficient(self) -> float:
+    @staticmethod
+    def get_probability(part: Union[int, float], whole: Union[int, float]) -> float:
         """
-        Gets a probability value, which is determined from the number of bycatch words identified.
-
-        Returns:
-            float: The preliminary score before being weighted.
-
-        Example:
-        >>> bycatch_coefficient = (1 + 1) / (1 + (2 - 0.95))
-        2 / 1 + 1.05
-        2 / 2.05
-        0.97
-        """
-        return (self.neg_part + 1) / (self.neg_part + (2 - self.s))
-
-    def get_weight(self, bycatch_coefficient: float) -> float:
-        """
-        Returns the weight as a result of the implicature score and bycatch coefficient.
+        Calculates the probability of an event occurring based on the part and the whole.
 
         Args:
-            bycatch_coefficient (float): A probability that is determined from the number of bycatch words identified.
+            part (int or float): The part that represents the event of interest.
+            whole (int or float): The total amount or size.
 
         Returns:
-            float: The weight as a result of the implicature score and bycatch coefficient.
+            float: The probability of the event occurring.
 
         Example:
-        >>> weight = 0.9 * 0.97
-        0.873
+        >>> RelevanceCalculator.get_probability(2, 4)
+        0.25
         """
-        return self.implicature_score * bycatch_coefficient
+        return (part / whole) ** part
 
     @classmethod
     def from_dict(cls, dict_input: dict):
