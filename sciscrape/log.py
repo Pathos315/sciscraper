e5a9d33863c8a19924f3dfea8dc7e8e2b79647d7
John Fallot <john.fallot@gmail.com>
2024-02-18 19:35:14 -0500

    logging changes

--- sciscrape/log.py
+++ sciscrape/log.py
@@ -5,7 +5,8 @@ from pathlib import Path
 
 logger = logging.getLogger("sciscraper")
 
-logging_configs = Path("logging_config.json").resolve()
+logging_configs_path = Path("logging_config.json")
+logging_configs = logging_configs_path.resolve()
 with open(logging_configs) as f_in:
     log_config = json.load(f_in)
-logging.config.dictConfig(log_config)
+    logging.config.dictConfig(log_config)
